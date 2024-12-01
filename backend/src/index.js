f90f10846cde664d6b6f60d1aa7105b381193396
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2024-12-01 16:27:27 +0530

    project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,6 +10,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 // const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json()); //this can help to take user entered value
 app.use(cookieParser());
 app.use(
@@ -19,6 +21,12 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log("server is running on port:", PORT);
   connectDB();
